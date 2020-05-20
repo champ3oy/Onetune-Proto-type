94632d610712e9e252c6484371117290c056c514
Cirlorm <akotosel6@gmail.com>
2020-05-20 17:21:45 +0000

    Update Firebase.js

--- src/Fire/Firebase.js
+++ src/Fire/Firebase.js
@@ -1,14 +1,16 @@
 import firebase from "firebase";
 
+// Use your firebase config here
+
 const config = {
-    apiKey: "AIzaSyBX7AJzgtdpfn1MDs2fzgrPdEPp7l4Ut50",
-    authDomain: "onetuneplus.firebaseapp.com",
-    databaseURL: "https://onetuneplus.firebaseio.com",
-    projectId: "onetuneplus",
-    storageBucket: "onetuneplus.appspot.com",
-    messagingSenderId: "944885545752",
-    appId: "1:944885545752:web:fdc3abcb6ca2ad6ba13c6d",
-    measurementId: "G-KHPE4V1MLG"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 // Initialize Firebase
 Fire = firebase.initializeApp(config);
